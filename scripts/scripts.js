5a1bc16cc0c315b65758fc59b52634d0ff551e32
Henrique Luís Oliveira Marques <henriquestudo@outlook.com>
2021-04-27 23:10:48 -0300

    Dev Snake

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,58 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box,
+};
+
+let direction = "right";
 
 function createBG() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-createBG();
+function createSnake() {
+  for (let i = 0; i < snake.length; i++) {
+    context.fillStyle = "green";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
+function startGame() {
+  createBG();
+  createSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction == "up") {
+    snakeY -= box;
+  }
+
+  if (direction == "right") {
+    snakeX += box;
+  }
+
+  if (direction == "down") {
+    snakeY += box;
+  }
+
+  if (direction == "left") {
+    snakeX -= box;
+  }
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
+  snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
